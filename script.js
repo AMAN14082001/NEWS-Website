33682c95f2b03e7252ebd467d13d0476f897bb42
AMAN14082001 <amanz1x14082001@gmail.com>
2025-05-01 22:18:24 +0530

    javascript fetchnews function const res content

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const url = "https;//newsapi.org/v2/everything?q=";
 window.addEventListener("load", () => fetchNews("India"));
 
 async function fetchNews(query) {
-    
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
 }
