a63102d956399eb58cfcf6b75e79f729835edc32
AMAN14082001 <amanz1x14082001@gmail.com>
2025-05-20 22:30:45 +0530

    newsTitle innerHTML article content

--- script.js
+++ script.js
@@ -30,5 +30,5 @@ function fillDataInCard(cardClone, article) {
     const newsDesc = cardClone.querySelector('news-desc');
 
     newsImg.src = article.urlToImage;
-    newsTitle.innerHTML = article.
+    newsTitle.innerHTML = article.title;
 }
