1093b9d05267c56d4f5261c7f3eb41da7a51c911
AMAN14082001 <amanz1x14082001@gmail.com>
2025-05-24 21:59:11 +0530

    const date new date article tolocalstring Template

--- script.js
+++ script.js
@@ -33,5 +33,5 @@ function fillDataInCard(cardClone, article) {
     newsTitle.innerHTML = article.title;
     newsDesc.innerHTML = article.description;
 
-    const date = new Date(article.publishedAt);
+    const date = new Date(article.publishedAt).toLocaleString();
 }
