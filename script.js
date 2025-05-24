d99b8df796ec9ae5617d320ad431ec86a7d7cfa6
AMAN14082001 <amanz1x14082001@gmail.com>
2025-05-24 22:00:51 +0530

    const date new date article tolocalstring timezone Template

--- script.js
+++ script.js
@@ -33,5 +33,7 @@ function fillDataInCard(cardClone, article) {
     newsTitle.innerHTML = article.title;
     newsDesc.innerHTML = article.description;
 
-    const date = new Date(article.publishedAt).toLocaleString("en-US");
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: 
+    });
 }
