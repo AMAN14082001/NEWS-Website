6927da672119670629ddf0d84a731251e5375085
AMAN14082001 <amanz1x14082001@gmail.com>
2025-05-28 21:55:09 +0530

    cardClone firstElementChild addeventlistener click content

--- script.js
+++ script.js
@@ -39,5 +39,5 @@ function fillDataInCard(cardClone, article) {
 
     newsSource.innerHTML = `${article.source.name} - ${date}`;
 
-    cardClone.firstElementChild.addEventListener()
+    cardClone.firstElementChild.addEventListener('click')
 }
